29514961c77b62c2f731447ef93bfd36fe004b7f
Iryna <your@mail.com>
2024-05-15 11:52:37 +0300

    Update test.yml

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -7,13 +7,13 @@ class Car:
 
 class CarWashStation:
     def __init__(self, distance_from_city_center: float, clean_power: int,
-                 average_rating: float, count_of_ratings: int):
+                 average_rating: float, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
@@ -31,10 +31,10 @@ class CarWashStation:
         )
         return washing_price
 
-    def wash_single_car(self, car: Car):
+    def wash_single_car(self, car: Car) -> None:
         car.clean_mark = self.clean_power
 
-    def rate_service(self, rating: float):
+    def rate_service(self, rating: float) -> None:
         self.average_rating = round(
             ((self.average_rating * self.count_of_ratings) + rating)
             / (self.count_of_ratings + 1), 1)
